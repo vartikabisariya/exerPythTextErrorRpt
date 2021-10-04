609e432a91f3842da4dc36e0ee3b70c6405e4827
cmcntsh <32034299+cmcntsh@users.noreply.github.com>
2021-10-03 18:11:23 -0600

    Add files via upload

---
+++ testArgs.py
@@ -0,0 +1,13 @@
+import sys
+ 
+# Print total number of arguments
+print ('Total number of arguments:', format(len(sys.argv)))
+ 
+# Print all arguments
+print ('Argument List:', str(sys.argv))
+ 
+# Print arguments one by one
+print ('First argument:',  str(sys.argv[0]))
+print ('Second argument:',  str(sys.argv[1]))
+print ('Third argument:',  str(sys.argv[2]))
+print ('Fourth argument:',  str(sys.argv[3]))
